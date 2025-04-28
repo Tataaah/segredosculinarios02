cb91c1a67d381aaf94bfaf27978308963a0cc4fc
Tataaah <tataspocket@gmail.com>
2025-04-28 18:02:24 -0300

    Delete src directory

--- src/utils/date.ts
+++
@@ -1,9 +0,0 @@
-export const getCurrentDate = (): string => {
-  const options: Intl.DateTimeFormatOptions = { 
-    day: '2-digit', 
-    month: '2-digit', 
-    year: 'numeric' 
-  };
-  
-  return new Date().toLocaleDateString('pt-BR', options);
-};
--- src/utils/scrollUtils.ts
+++
@@ -1,9 +0,0 @@
-export const scrollToSection = (id: string): void => {
-  const element = document.getElementById(id);
-  if (element) {
-    element.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
-  }
-};
